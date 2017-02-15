ba7640962602b5bfc5ab6aced6695d346ad79574
Sirinova <www.ul13@mail.ru>
2017-02-15 21:12:46 +0500

    Removed privide

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,9 +35,14 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should have not a custom page title" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title(' | Help')
     end
   end
 
@@ -47,10 +52,16 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the title 'About'" do
+    it "should have the base title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
+      expect(page).to have_title("#{base_title}")
     end
+
+    it "should have not a custom page title" do
+      visit '/static_pages/about'
+      expect(page).not_to have_title(' | About Us')
+    end
+
   end
 
   describe "Contact" do
@@ -59,9 +70,14 @@ describe "Static pages" do
       expect(page).to have_content('Contact')
     end
 
-    it "should have the title 'Contact'" do
+    it "should have the base title" do
       visit'/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should have not_to a custom page title" do
+      visit '/static_pages/contact'
+      expect(page).not_to have_title(' | About Us')
     end
   end
 end
