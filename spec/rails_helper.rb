79113bf4df8af544292a3e9a1301572f5c694024
Sirinova <www.ul13@mail.ru>
2017-02-19 19:52:37 +0500

    Finish layout and routes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include ApplicationHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
@@ -34,7 +35,6 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
   config.include Capybara::DSL
-  
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+def full_title(page_tittle)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_tittle.empty?
+    base_title
+  else
+    "#{base_title} | #{page_tittle}"
+  end  
+end
