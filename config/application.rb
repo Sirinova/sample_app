1efb2229406eaa4607d28c2267c0788b9bd6da6e
Sirinova <www.ul13@mail.ru>
2017-02-21 22:22:49 +0500

    Make a basic User model (including secure passwords)

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module SampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    
   end
 end
